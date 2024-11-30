16d8b27241d6ad2c5fcd5010dbb03e86f4ddc735
SettingDust <settingdust@gmail.com>
2024-11-30 20:11:39 +0800

    feat(lazy-entity-renderers): add compat to quark

---
+++ lexforge/lexforge-mod/src/main/java/settingdust/lazyyyyy/mixin/forge/lazy_entity_renderers/quark/UsesForCursesModule_ClientMixin.java
@@ -0,0 +1,22 @@
+package settingdust.lazyyyyy.mixin.forge.lazy_entity_renderers.quark;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.moulberry.mixinconstraints.annotations.IfModLoaded;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.world.entity.EntityType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.violetmoon.quark.content.client.module.UsesForCursesModule;
+import org.violetmoon.zeta.client.event.load.ZAddModelLayers;
+import settingdust.lazyyyyy.minecraft.DummyLivingEntityRenderer;
+
+@IfModLoaded("quark")
+@Mixin(UsesForCursesModule.Client.class)
+public class UsesForCursesModule_ClientMixin {
+    @WrapMethod(method = "modelLayers")
+    private void lazyyyyy$avoidAddIfDummy(final ZAddModelLayers event, final Operation<Void> original) {
+        var renderer = (EntityRenderer<?>) event.getRenderer(EntityType.ARMOR_STAND);
+        if (renderer instanceof DummyLivingEntityRenderer) return;
+        original.call(event);
+    }
+}
