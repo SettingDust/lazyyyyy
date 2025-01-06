2f97e56f9ab2d54310bd5686d6e39afaee4e14b3
SettingDust <settingdust@gmail.com>
2025-01-06 22:04:16 +0800

    fix(lazy-entity-renderers): fix duplicating layers if mod iterate over all the renderers
    https://github.com/Magistu/Epic-Knights/blob/88be9f811b1cd3702061b9dd681e10fcb3c9f5dc/forge/src/main/java/com/magistuarmory/client/render/forge/ModRenderImpl.java

--- lexforge/lexforge-mod/src/main/kotlin/settingdust/lazyyyyy/forge/Entrypoint.kt
+++ lexforge/lexforge-mod/src/main/kotlin/settingdust/lazyyyyy/forge/Entrypoint.kt
@@ -1,28 +1,51 @@
 package settingdust.lazyyyyy.forge
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
+import net.minecraft.client.Minecraft
 import net.minecraftforge.client.event.EntityRenderersEvent
 import net.minecraftforge.fml.ModLoader
 import net.minecraftforge.fml.common.Mod
 import settingdust.lazyyyyy.Lazyyyyy
 import settingdust.lazyyyyy.minecraft.LazyEntityRenderer
 import settingdust.lazyyyyy.minecraft.LazyPlayerRenderer
+import settingdust.lazyyyyy.minecraft.playerRenderers
 
 @Mod(Lazyyyyy.ID)
 object LazyyyyyForge {
     init {
-        Lazyyyyy.scope.launch {
+        CoroutineScope(SupervisorJob() + Dispatchers.Default).launch {
             launch {
                 LazyEntityRenderer.onAddLayer.collect { (type, context, renderer) ->
+                    val originalRenderers = Minecraft.getInstance().entityRenderDispatcher.renderers
+                    Minecraft.getInstance().entityRenderDispatcher.renderers = mutableMapOf(type to renderer)
                     ModLoader.get()
-                        .postEvent(EntityRenderersEvent.AddLayers(mapOf(type to renderer), emptyMap(), context))
+                        .postEvent(
+                            EntityRenderersEvent.AddLayers(
+                                Minecraft.getInstance().entityRenderDispatcher.renderers,
+                                emptyMap(),
+                                context
+                            )
+                        )
+                    Minecraft.getInstance().entityRenderDispatcher.renderers = originalRenderers
                 }
             }
 
             launch {
                 LazyPlayerRenderer.onAddLayer.collect { (type, context, renderer) ->
+                    val originalRenderers = Minecraft.getInstance().entityRenderDispatcher.playerRenderers
+                    Minecraft.getInstance().entityRenderDispatcher.playerRenderers = mutableMapOf(type to renderer)
                     ModLoader.get()
-                        .postEvent(EntityRenderersEvent.AddLayers(emptyMap(), mapOf(type to renderer), context))
+                        .postEvent(
+                            EntityRenderersEvent.AddLayers(
+                                emptyMap(),
+                                Minecraft.getInstance().entityRenderDispatcher.playerRenderers,
+                                context
+                            )
+                        )
+                    Minecraft.getInstance().entityRenderDispatcher.playerRenderers = originalRenderers
                 }
             }
         }
