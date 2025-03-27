2dbcf64638ad34c1f97e0f4024ee561c6dfa4d51
SettingDust <settingdust@gmail.com>
2025-03-27 12:33:22 +0800

    chore(async-model-baking): disable by default since it's lagging fps

--- xplat/src/main/kotlin/settingdust/lazyyyyy/LazyyyyyMixinPlugin.kt
+++ xplat/src/main/kotlin/settingdust/lazyyyyy/LazyyyyyMixinPlugin.kt
@@ -21,7 +21,7 @@ class LazyyyyyMixinPlugin : ConstraintsMixinPlugin() {
         private set
 
     val defaultConfig = mapOf<String, Boolean>(
-        "async_model_baking" to true,
+        "async_model_baking" to false,
         "axiom.async_check_commercial" to true,
         "entity_sound_features.async_sound_events" to true,
         "lazy_entity_renderers" to true,
