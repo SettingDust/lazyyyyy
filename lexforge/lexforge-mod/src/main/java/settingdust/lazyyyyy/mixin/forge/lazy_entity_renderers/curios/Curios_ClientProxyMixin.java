469441f6b8b910e62568369ed433ba9ef2963c27
SettingDust <settingdust@gmail.com>
2025-04-29 17:37:03 +0800

    perf(lazy-entity-renderers): avoid curios load the renderers multiple times

---
+++ lexforge/lexforge-mod/src/main/java/settingdust/lazyyyyy/mixin/forge/lazy_entity_renderers/curios/Curios_ClientProxyMixin.java
@@ -0,0 +1,17 @@
+package settingdust.lazyyyyy.mixin.forge.lazy_entity_renderers.curios;
+
+import com.moulberry.mixinconstraints.annotations.IfModLoaded;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import top.theillusivec4.curios.Curios;
+
+@IfModLoaded(Curios.MODID)
+@Mixin(Curios.ClientProxy.class)
+public class Curios_ClientProxyMixin {
+    @Redirect(
+        method = "addLayers",
+        at = @At(value = "INVOKE", target = "Ltop/theillusivec4/curios/api/client/CuriosRendererRegistry;load()V")
+    )
+    private static void lazyyyyy$avoidLoad() {}
+}
